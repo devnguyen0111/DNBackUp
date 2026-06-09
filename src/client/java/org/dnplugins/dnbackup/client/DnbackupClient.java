1b176c742fe17c8067af3200face63764a2784b6
Nhứt Nguyên <kieuphuongcsgo@gmail.com>
2026-06-10 01:31:01 +0700

    feat: initialize DNBackUp Minecraft plugin project with core backup management and configuration structures

---
+++ src/client/java/org/dnplugins/dnbackup/client/DnbackupClient.java
@@ -0,0 +1,10 @@
+package org.dnplugins.dnbackup.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DnbackupClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/dnplugins/dnbackup/Dnbackup.java
@@ -0,0 +1,10 @@
+package org.dnplugins.dnbackup;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Dnbackup implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
